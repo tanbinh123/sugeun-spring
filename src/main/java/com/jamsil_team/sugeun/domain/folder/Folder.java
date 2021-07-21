2e30ff5c3edac9c3d6e45e712949bf6f7a60cdb1
hyeongwoo-Lee <lhw970804@naver.com>
2021-07-22 00:03:05 +0900

    머지

--- src/main/java/com/jamsil_team/sugeun/domain/folder/Folder.java
+++ src/main/java/com/jamsil_team/sugeun/domain/folder/Folder.java
@@ -41,8 +41,7 @@ public class Folder {
     public void changeFolderName(String folderName){
         this.folderName = folderName;
     }
-
-
+    
     public FolderDTO toDTO(){
 
         FolderDTO folderDTO;
